c0d7ab8d1bca457f9d77e85d99bdedb1d79fd4c3
Philipp Meier <philipp.meier@codecentric.de>
2015-10-16 16:46:45 +0200

    implemented

--- gen-certs/gen-certs.sh
+++ gen-certs/gen-certs.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+# following: https://coreos.com/kubernetes/docs/latest/openssl.html
+
+# needs publicly routable MASTER_HOST env set
+# uses default K8S_SERVICE_IP
+K8S_SERVICE_IP=10.3.0.1 mo openssl.cnf.mo > openssl.cnf
+
+# cluster root ca to sign other certs
+openssl genrsa -out ca-key.pem 2048
+openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj "/CN=kube-ca"
+
+# api server keypair
+openssl genrsa -out apiserver-key.pem 2048
+openssl req -new -key apiserver-key.pem -out apiserver.csr -subj "/CN=kube-apiserver" -config openssl.cnf
+openssl x509 -req -in apiserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out apiserver.pem -days 365 -extensions v3_req -extfile openssl.cnf
+
+# worker keypair
+openssl genrsa -out worker-key.pem 2048
+openssl req -new -key worker-key.pem -out worker.csr -subj "/CN=kube-worker"
+openssl x509 -req -in worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out worker.pem -days 365
+
+# cluster administrator keypair
+openssl genrsa -out admin-key.pem 2048
+openssl req -new -key admin-key.pem -out admin.csr -subj "/CN=kube-admin"
+openssl x509 -req -in admin.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out admin.pem -days 365
+
+mv *.pem certs
